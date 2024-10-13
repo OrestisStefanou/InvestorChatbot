84f0ef750ae45e71eb2f691e0009b5422bbf1a37
OrestisStefanou <orestaras1997@outlook.com>
2024-10-13 18:14:22 +0300

    Scrape foracast data

--- stock_forecasts.py
+++ stock_forecasts.py
@@ -1,6 +1,6 @@
 import httpx
 
-url = 'https://stockanalysis.com/stocks/aapl/forecast/__data.json'
+url = 'https://stockanalysis.com/stocks/nvda/forecast/__data.json'
 response = httpx.get(url)
 raw_data = response.json()
 
@@ -8,10 +8,7 @@ nodes = raw_data['nodes']
 data = nodes[2]['data']
 data_map = data[0]
 
-print(data_map)
-print(data[187])    # targets data
-print(data[1])  # estimates data
-
+# ESTIMATES SCRAPING
 estimates_data_index = data_map['estimates']
 estimates_table_data_index = data[estimates_data_index]['table']
 quarterly_estimates_data_index = data[estimates_table_data_index]['quarterly']
@@ -20,7 +17,6 @@ quarterly_estimates_data = {}
 for estimation_field, estimation_field_idx in quarterly_estimates_data_map.items():
     if estimation_field == 'lastDate':
         continue
-    print(estimation_field, estimation_field_idx)
     estimation_field_values = []
     for field_value_index in data[estimation_field_idx]:
         if data[field_value_index] == '[PRO]':
@@ -31,5 +27,14 @@ for estimation_field, estimation_field_idx in quarterly_estimates_data_map.items
     quarterly_estimates_data[estimation_field] = estimation_field_values
 
 estimations_doc = [dict(zip(quarterly_estimates_data.keys(), values)) for values in zip(*quarterly_estimates_data.values())]
-print('-' * 100)
-print(estimations_doc)
+
+# TARGET PRICE SCRAPING
+target_data_index = data_map['targets']
+target_data_map = data[target_data_index]
+target_price_keys = ['average', 'high', 'low', 'median']
+target_price_doc = {}
+for target_key in target_price_keys:
+    target_value_index = target_data_map[target_key]
+    target_price_doc[target_key] = data[target_value_index]
+
+print(target_price_doc)
