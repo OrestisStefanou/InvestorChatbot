db6417a0e80418edd5c4ffe2a592a2f43d12c845
Orestis Stefanou <o.stefanou@withplum.com>
2024-10-31 18:26:10 +0200

    Make helper function to get financial statement data

--- balance_sheet.go
+++ balance_sheet.go
@@ -64,92 +64,180 @@ type BalanceSheet struct {
 	TradingAssetSecurities         float64
 }
 
-func get_balance_sheets() {
+func get_financial_statement_data() []map[string]interface{} {
+	statement_data := make(map[string][]interface{})
+	statement_data_slice := []map[string]interface{}{}
 	url := "https://stockanalysis.com/stocks/aapl/financials/balance-sheet/__data.json?p=quarterly"
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error fetching data:", err)
-		return
+		return statement_data_slice
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error reading response body:", err)
-		return
+		return statement_data_slice
 	}
 
 	var rawData map[string]interface{}
 	if err := json.Unmarshal(body, &rawData); err != nil {
 		fmt.Println("Error unmarshalling JSON:", err)
-		return
+		return statement_data_slice
 	}
 
 	// Extract "nodes" from rawData
 	nodes, ok := rawData["nodes"].([]interface{})
 	if !ok || len(nodes) < 3 {
 		fmt.Println("Unexpected structure in 'nodes'")
-		return
+		return statement_data_slice
 	}
 
 	// Access the second element in "nodes" which contains the data we are interested in
 	nodeData, ok := nodes[2].(map[string]interface{})
 	if !ok {
 		fmt.Println("Unexpected structure in 'nodes[2]'")
-		return
+		return statement_data_slice
 	}
 
 	data, ok := nodeData["data"].([]interface{})
 	if !ok {
 		fmt.Println("Unexpected structure in 'data'")
-		return
+		return statement_data_slice
 	}
 
 	dataMap, ok := data[0].(map[string]interface{})
 	if !ok {
 		fmt.Println("Unexpected structure in 'data[0]'")
-		return
+		return statement_data_slice
 	}
 
 	financialDataIndex, ok := dataMap["financialData"].(float64)
 	if !ok {
 		fmt.Println("Unexpected structure for 'financialData'")
-		return
+		return statement_data_slice
 	}
 
 	// Retrieve data map at financial data index
-	balanceSheetDataMap, ok := data[int(financialDataIndex)].(map[string]interface{})
+	statementDataMap, ok := data[int(financialDataIndex)].(map[string]interface{})
 	if !ok {
 		fmt.Println("Unexpected structure in balance sheet data map")
-		return
+		return statement_data_slice
 	}
 
-	balanceSheetData := make(map[string][]interface{})
-	for field, fieldIndex := range balanceSheetDataMap {
+	for field, fieldIndex := range statementDataMap {
 		fieldIndexFloat, ok := fieldIndex.(float64)
 		if !ok {
 			fmt.Println("Unexpected index type in fieldIndex")
-			return
+			return statement_data_slice
 		}
 		fieldValues := []interface{}{}
 		for _, index := range data[int(fieldIndexFloat)].([]interface{}) {
 			indexFloat, ok := index.(float64)
 			if !ok {
 				fmt.Println("Unexpected type in field values index")
-				return
+				return statement_data_slice
 			}
 			fieldValues = append(fieldValues, data[int(indexFloat)])
 		}
-		balanceSheetData[field] = fieldValues
+		statement_data[field] = fieldValues
 	}
 
 	// Converting the map of slices into a slice of maps to resemble final structure
-	result := []map[string]interface{}{}
-	for i := 0; i < len(balanceSheetData["datekey"]); i++ {
+	for i := 0; i < len(statement_data["datekey"]); i++ {
 		record := make(map[string]interface{})
-		for key, values := range balanceSheetData {
+		for key, values := range statement_data {
 			record[key] = values[i]
 		}
+		statement_data_slice = append(statement_data_slice, record)
+	}
+	fmt.Println(statement_data_slice)
+	return statement_data_slice
+}
+
+func get_balance_sheets() {
+	// url := "https://stockanalysis.com/stocks/aapl/financials/balance-sheet/__data.json?p=quarterly"
+	// resp, err := http.Get(url)
+	// if err != nil {
+	// 	fmt.Println("Error fetching data:", err)
+	// 	return
+	// }
+	// defer resp.Body.Close()
+
+	// body, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	fmt.Println("Error reading response body:", err)
+	// 	return
+	// }
+
+	// var rawData map[string]interface{}
+	// if err := json.Unmarshal(body, &rawData); err != nil {
+	// 	fmt.Println("Error unmarshalling JSON:", err)
+	// 	return
+	// }
+
+	// // Extract "nodes" from rawData
+	// nodes, ok := rawData["nodes"].([]interface{})
+	// if !ok || len(nodes) < 3 {
+	// 	fmt.Println("Unexpected structure in 'nodes'")
+	// 	return
+	// }
+
+	// // Access the second element in "nodes" which contains the data we are interested in
+	// nodeData, ok := nodes[2].(map[string]interface{})
+	// if !ok {
+	// 	fmt.Println("Unexpected structure in 'nodes[2]'")
+	// 	return
+	// }
+
+	// data, ok := nodeData["data"].([]interface{})
+	// if !ok {
+	// 	fmt.Println("Unexpected structure in 'data'")
+	// 	return
+	// }
+
+	// dataMap, ok := data[0].(map[string]interface{})
+	// if !ok {
+	// 	fmt.Println("Unexpected structure in 'data[0]'")
+	// 	return
+	// }
+
+	// financialDataIndex, ok := dataMap["financialData"].(float64)
+	// if !ok {
+	// 	fmt.Println("Unexpected structure for 'financialData'")
+	// 	return
+	// }
+
+	// // Retrieve data map at financial data index
+	// balanceSheetDataMap, ok := data[int(financialDataIndex)].(map[string]interface{})
+	// if !ok {
+	// 	fmt.Println("Unexpected structure in balance sheet data map")
+	// 	return
+	// }
+
+	// balanceSheetData := make(map[string][]interface{})
+	// for field, fieldIndex := range balanceSheetDataMap {
+	// 	fieldIndexFloat, ok := fieldIndex.(float64)
+	// 	if !ok {
+	// 		fmt.Println("Unexpected index type in fieldIndex")
+	// 		return
+	// 	}
+	// 	fieldValues := []interface{}{}
+	// 	for _, index := range data[int(fieldIndexFloat)].([]interface{}) {
+	// 		indexFloat, ok := index.(float64)
+	// 		if !ok {
+	// 			fmt.Println("Unexpected type in field values index")
+	// 			return
+	// 		}
+	// 		fieldValues = append(fieldValues, data[int(indexFloat)])
+	// 	}
+	// 	balanceSheetData[field] = fieldValues
+	// }
+
+	balanceSheetData := get_financial_statement_data()
+	for i := 0; i < len(balanceSheetData); i++ {
+		record := balanceSheetData[i]
 		// Marshal the map to JSON
 		jsonData, err := json.Marshal(record)
 		if err != nil {
@@ -164,13 +252,7 @@ func get_balance_sheets() {
 			return
 		}
 		fmt.Println(balanceSheetRecord)
-		result = append(result, record)
 	}
-
-	// Output final data
-	fmt.Println(len(result))
-	fmt.Println("--------------------------")
-	fmt.Println(result[0]["totalcash"])
 }
 
 func main() {
