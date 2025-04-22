262d482a282b7f1e308ef80df5aab079e193b4a5
OrestisStefanou <orestaras1997@outlook.com>
2025-04-22 12:04:13 +0300

    Competition avg fincancial ratios

--- cmd/temp/main.go
+++ cmd/temp/main.go
@@ -1,42 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"investbot/pkg/config"
-	"investbot/pkg/marketDataScraper"
-	"investbot/pkg/openAI"
-	"investbot/pkg/services"
-	"log"
-)
-
 func main() {
-	conf, _ := config.LoadConfig()
-	openAiClient, _ := openAI.NewOpenAiClient(conf.OpenAiKey, conf.OpenAiBaseUrl)
-	llm, err := openAI.NewOpenAiLLM(conf.OpenAiModelName, openAiClient, float64(conf.BaseLlmTemperature))
+	// conf, _ := config.LoadConfig()
+	// openAiClient, _ := openAI.NewOpenAiClient(conf.OpenAiKey, conf.OpenAiBaseUrl)
+	// llm, err := openAI.NewOpenAiLLM(conf.OpenAiModelName, openAiClient, float64(conf.BaseLlmTemperature))
 
-	scraper := marketDataScraper.MarketDataScraper{}
+	// scraper := marketDataScraper.MarketDataScraper{}
 
-	financialRatios, err := scraper.GetFinancialRatios("wlds")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// financialRatios, err := scraper.GetFinancialRatios("wlds")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	fmt.Println(financialRatios)
+	// fmt.Println(financialRatios)
 
-	stockOverviewRag, err := services.NewStockOverviewRag(llm, scraper)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// stockOverviewRag, err := services.NewStockOverviewRag(llm, scraper)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	stockProfile, err := scraper.GetStockProfile("AMD")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// stockProfile, err := scraper.GetStockProfile("AMD")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	competitors, err := stockOverviewRag.GetStockCompetitors(stockProfile, "AMD")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// competitors, err := stockOverviewRag.GetStockCompetitors(stockProfile, "AMD")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	fmt.Println(competitors)
+	// fmt.Println(competitors)
 }
--- pkg/services/stock_overview_service.go
+++ pkg/services/stock_overview_service.go
@@ -15,6 +15,25 @@ type StockOverviewDataService interface {
 }
 
 type competitorsAvgFinancialRatios struct {
+	AvgPe                float64
+	AvgPb                float64
+	AvgPfcf              float64
+	AvgEvEbitda          float64
+	AvgEvEbit            float64
+	AvgEvFcf             float64
+	AvgDebtEquity        float64
+	AvgDebtEbitda        float64
+	AvgDebtFcf           float64
+	AvgAssetTurnover     float64
+	AvgInventoryTurnover float64
+	AvgQuickRatio        float64
+	AvgCurrentRatio      float64
+	AvgRoe               float64
+	AvgRoa               float64
+	AvgRoic              float64
+	AvgEarningYield      float64
+	AvgFcfYield          float64
+	AvgDividendYield     float64
 }
 
 type stockOverviewContext struct {
@@ -35,7 +54,8 @@ func NewStockOverviewRag(llm Llm, stockOverviewDataService StockOverviewDataServ
 	return &StockOverviewRag{llm: llm, dataService: stockOverviewDataService}, nil
 }
 
-func (rag StockOverviewRag) GetStockCompetitors(stockProfile domain.StockProfile, symbol string) ([]domain.IndustryStock, error) {
+// TODO: Should this be a method on the dataService?
+func (rag StockOverviewRag) getStockCompetitors(stockProfile domain.StockProfile, symbol string) ([]domain.IndustryStock, error) {
 	// Fetch the industry stocks
 	industryStocks, err := rag.dataService.GetIndustryStocks(stockProfile.IndustryUrlName)
 	if err != nil {
@@ -74,8 +94,58 @@ func (rag StockOverviewRag) GetStockCompetitors(stockProfile domain.StockProfile
 	return competitors, nil
 }
 
-func (rag StockOverviewRag) getCompetitorsAvgFinancialRatios(symbols []string) (competitorsAvgFinancialRatios, error) {
-	return competitorsAvgFinancialRatios{}, nil
+func (rag StockOverviewRag) getCompetitorsAvgFinancialRatios(competitors []domain.IndustryStock) (competitorsAvgFinancialRatios, error) {
+	totalRatios := domain.FinancialRatios{}
+
+	for _, competitor := range competitors {
+		financialRatios, err := rag.dataService.GetFinancialRatios(competitor.Symbol)
+		if err != nil {
+			return competitorsAvgFinancialRatios{}, err
+		}
+
+		totalRatios.Pe += financialRatios[0].Pe
+		totalRatios.Pb += financialRatios[0].Pb
+		totalRatios.Pfcf += financialRatios[0].Pfcf
+		totalRatios.EvEbitda += financialRatios[0].EvEbitda
+		totalRatios.EvEbit += financialRatios[0].EvEbit
+		totalRatios.EvFcf += financialRatios[0].EvFcf
+		totalRatios.DebtEquity += financialRatios[0].DebtEquity
+		totalRatios.DebtEbitda += financialRatios[0].DebtEbitda
+		totalRatios.DebtFcf += financialRatios[0].DebtFcf
+		totalRatios.AssetTurnover += financialRatios[0].AssetTurnover
+		totalRatios.InventoryTurnover += financialRatios[0].InventoryTurnover
+		totalRatios.QuickRatio += financialRatios[0].QuickRatio
+		totalRatios.CurrentRatio += financialRatios[0].CurrentRatio
+		totalRatios.Roe += financialRatios[0].Roe
+		totalRatios.Roa += financialRatios[0].Roa
+		totalRatios.Roic += financialRatios[0].Roic
+		totalRatios.EarningsYield += financialRatios[0].EarningsYield
+		totalRatios.FcfYield += financialRatios[0].FcfYield
+		totalRatios.DividendYield += financialRatios[0].DividendYield
+	}
+
+	avgRatios := competitorsAvgFinancialRatios{}
+	avgRatios.AvgPe = totalRatios.Pe / float64(len(competitors))
+	avgRatios.AvgPb = totalRatios.Pb / float64(len(competitors))
+	avgRatios.AvgPfcf = totalRatios.Pfcf / float64(len(competitors))
+	avgRatios.AvgEvEbitda = totalRatios.EvEbitda / float64(len(competitors))
+	avgRatios.AvgEvEbit = totalRatios.EvEbit / float64(len(competitors))
+	avgRatios.AvgEvFcf = totalRatios.EvFcf / float64(len(competitors))
+	avgRatios.AvgDebtEquity = totalRatios.DebtEquity / float64(len(competitors))
+	avgRatios.AvgDebtEbitda = totalRatios.DebtEbitda / float64(len(competitors))
+	avgRatios.AvgDebtFcf = totalRatios.DebtFcf / float64(len(competitors))
+	avgRatios.AvgAssetTurnover = totalRatios.AssetTurnover / float64(len(competitors))
+	avgRatios.AvgInventoryTurnover = totalRatios.InventoryTurnover / float64(len(competitors))
+	avgRatios.AvgQuickRatio = totalRatios.QuickRatio / float64(len(competitors))
+	avgRatios.AvgCurrentRatio = totalRatios.CurrentRatio / float64(len(competitors))
+	avgRatios.AvgRoe = totalRatios.Roe / float64(len(competitors))
+	avgRatios.AvgRoa = totalRatios.Roa / float64(len(competitors))
+	avgRatios.AvgRoic = totalRatios.Roic / float64(len(competitors))
+	avgRatios.AvgEarningYield = totalRatios.EarningsYield / float64(len(competitors))
+	avgRatios.AvgFcfYield = totalRatios.FcfYield / float64(len(competitors))
+	avgRatios.AvgDividendYield = totalRatios.DividendYield / float64(len(competitors))
+
+	return avgRatios, nil
 }
 
 func (rag StockOverviewRag) createRagContext(symbols []string) (string, error) {
@@ -103,6 +173,21 @@ func (rag StockOverviewRag) createRagContext(symbols []string) (string, error) {
 			return "", &DataServiceError{Message: fmt.Sprintf("GetStockForecast failed: %s", err)}
 		}
 		context.stockForecast = stockForecast
+
+		competitors, err := rag.getStockCompetitors(stockProfile, symbol)
+		if err != nil {
+			fmt.Printf("getStockCompetitors failed: %s", err)
+			competitors = []domain.IndustryStock{}
+		}
+		avgFinancialRatios, err := rag.getCompetitorsAvgFinancialRatios(competitors)
+		if err != nil {
+			fmt.Printf("getCompetitorsAvgFinancialRatios failed: %s", err)
+			avgFinancialRatios = competitorsAvgFinancialRatios{}
+		}
+		context.competitorsAvgFinancialRatios = avgFinancialRatios
+
+		// TODO: REMOVE THIS
+		fmt.Printf("\n\ncontext: %+v\n\n", context)
 
 		ragContext = append(ragContext, context)
 	}
