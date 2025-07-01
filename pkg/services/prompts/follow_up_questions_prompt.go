193b2ccf27cc1e3cea1a962d9fb83ff95a08b438
OrestisStefanou <orestaras1997@outlook.com>
2025-07-01 10:41:46 +0200

    Update follow up questions prompt

--- pkg/services/prompts/follow_up_questions_prompt.go
+++ pkg/services/prompts/follow_up_questions_prompt.go
@@ -2,8 +2,7 @@ package prompts
 
 const FollowUpQuestionsPrompt = `
 You are an expert in investing! Your mission is given a conversation about investing to respond
-with %d follow up questions that make sense to ask given the context of the conversation. Your 
-audience is mostly beginner level investors so take this into consideration. The follow up questions that you return
+with %d follow up questions that make sense to ask given the context of the conversation. The follow up questions that you return
 will be given to another investing expert so take that into consideration regarding the phrasing of those questions. So 
 basically you provide the users some follow up questions that they can ask another investing expert.
 
