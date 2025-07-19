73ae3cbf2b9cfa375cff9cbffbb92a2e7e18f3ce
OrestisStefanou <orestaras1997@outlook.com>
2025-07-19 14:07:44 +0300

    improved ui client

---
+++ ui/client_v2.py
@@ -0,0 +1,125 @@
+import streamlit as st
+import random
+import time
+import requests
+import json
+
+
+BASE_URL = "http://localhost:1323"
+USER_ID = "orestis_user_id"
+
+
+def create_session() -> str:
+    response = requests.post(f"{BASE_URL}/session")
+    session_id = response.json()["session_id"]
+    return session_id
+
+
+def extract_topic_and_tags(session_id: str, question: str) -> dict:
+    payload = {
+        "session_id": session_id,
+        "question": question,
+        "user_id": USER_ID,
+    }
+
+    response = requests.post(f"{BASE_URL}/chat/extract_topic_and_tags", json=payload)
+    if response.status_code != 200:
+        print(response.json())
+        raise Exception("Failed to extract topic and tags")
+    
+    return response.json()
+
+def stream_chat_response(
+    question: str,
+    session_id: str, 
+    topic: str,
+    topic_tags: dict=None,
+):
+    url = f"{BASE_URL}/chat"
+    payload = {
+        "question": question,
+        "topic": topic,
+        "session_id": session_id
+    }
+    
+    if topic_tags:
+        payload["topic_tags"] = topic_tags
+
+    headers = {"Content-Type": "application/json"}
+
+    try:
+        response = requests.post(url, json=payload, headers=headers, stream=True)
+
+        response.raise_for_status()  # Raise error for non-2xx responses
+
+        for chunk in response.iter_lines(decode_unicode=True):
+            if chunk:  # filter out keep-alive new lines
+                yield chunk
+
+    except requests.exceptions.RequestException as e:
+        yield f"[ERROR] {e}"
+
+
+def response_generator(session_id: str, question: str):
+    topic_and_tags = extract_topic_and_tags(
+        session_id=session_id,
+        question=question,
+    )
+
+    for chunk in stream_chat_response(
+        question=question,
+        session_id=session_id,
+        topic=topic_and_tags["topic"],
+        topic_tags=topic_and_tags["topic_tags"],
+    ):
+        try:
+            # Parse each chunk from JSON string literal to clean text
+            text = json.loads(chunk)
+            if text.strip():  # skip empty chunks
+                yield text
+        except Exception as e:
+            print(f"Chunk parse error: {e} | raw: {repr(chunk)}")
+            continue
+
+
+st.title("Investor Assistant chatbot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Initialize session
+if "session_id" not in st.session_state:
+    st.session_state.session_id = create_session()
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # # Display assistant response in chat message container
+    # with st.chat_message("assistant"):
+    #     response = st.write_stream(response_generator(session_id=st.session_state.session_id, question=prompt))
+    # # Add assistant response to chat history
+    # st.session_state.messages.append({"role": "assistant", "content": response})
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response_placeholder = st.empty()
+        full_response = ""
+
+        for chunk in response_generator(
+            session_id=st.session_state.session_id, question=prompt
+        ):
+            full_response += chunk
+            response_placeholder.markdown(full_response)
+
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
