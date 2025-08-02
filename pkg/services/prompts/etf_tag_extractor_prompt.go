9171756c05f39fba995abff42a189707eba07f62
OrestisStefanou <orestaras1997@outlook.com>
2025-08-02 12:44:47 +0300

    Compare ETFs

--- pkg/services/prompts/etf_tag_extractor_prompt.go
+++ pkg/services/prompts/etf_tag_extractor_prompt.go
@@ -11,15 +11,20 @@ Some context of the user asking the question is given below. You should take thi
 %+v
 
 ## Response instructions
-- Your response MUST BE a json parsable string with a key named 'etf_symbol' and value the string that will contain
-the etf symbol the conversation is about. In case the question is generic and not for a specific etf then return an empty 
-string for 'etf_symbol' key.
+- Your response MUST BE a json parsable string with a key named 'etf_symbols' and value an array of strings that will contain
+the etf symbols the conversation is about. In case the question is generic and not for a specific etf then return an empty 
+array for 'etf_symbols' key.
 
 - Your answer should focus on the last question of the conversation, for example if the first 5 messages are about Vanguard S&P 500 ETF
 but the last question is about Vanguard Growth ETF then your answer should contain the Vanguard Growth ETF symbol.
 
-Example response:
-{"etf_symbol":["VOO"]}
+Example response if the conversation is about Vanguard S&P 500 ETF:
+{"etf_symbols":["VOO"]}
+
+Example response if the conversation is about Vanguard S&P 500 ETF and Vanguard Growth ETF:
+{"etf_symbols":["VOO", "VUG"]}
+
+If the conversation is about the user portfolio then you should use the user context above for your response.
 
 # Conversation
 %+v
