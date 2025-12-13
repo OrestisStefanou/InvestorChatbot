26627115db7c22d737d9236ea8e6111c4ed797ed
OrestisStefanou <orestaras1997@outlook.com>
2025-12-13 16:43:49 +0200

    Get/update user context endpoints

--- agent_service/services/user_context.py
+++ agent_service/services/user_context.py
@@ -1,14 +1,17 @@
 from abc import ABC, abstractmethod
 
 from pydantic import BaseModel
-from pymongo import AsyncMongoClient
+from pymongo import (
+    AsyncMongoClient,
+    ReturnDocument,
+)
 
 # The naming of the fields is done to match mcp server schema
 class UserPortfolioHolding(BaseModel):
     assetclass: str
     symbol: str
     name: str
-    quantity: int
+    quantity: float
 
 class UserContext(BaseModel):
     userid: str
@@ -19,6 +22,9 @@ class UserContext(BaseModel):
 class UserContextAlreadyExistsError(Exception):
     pass
 
+
+class UserContextNotFoundError(Exception):
+    pass
 
 class UserContextService(ABC):
     @abstractmethod
@@ -42,7 +48,20 @@ class MongoDBUserContextService(UserContextService):
         user_profile: dict | None = None,
         user_portfolio: list[UserPortfolioHolding] | None = None,
     ) -> UserContext | None:
+        """
+        Create a new user context for the given user_id.
 
+        Args:
+            user_id: The user_id for which to create the user context.
+            user_profile: The user profile to be stored in the user context.
+            user_portfolio: The user portfolio to be stored in the user context.
+        
+        Raises:
+            UserContextAlreadyExistsError: If a user context for the given user_id already exists.
+
+        Returns:
+            The created user context.
+        """
         # Check if user context for user_id already exists
         existing_user_context = await self.collection.find_one({"userid": user_id})
         if existing_user_context:
@@ -55,3 +74,43 @@ class MongoDBUserContextService(UserContextService):
         )
         await self.collection.insert_one(user_context.model_dump())
         return user_context
+
+    async def get_user_context(self, user_id: str) -> UserContext | None:
+        """
+        Get the user context for the given user_id.
+
+        Args:
+            user_id: The user_id for which to get the user context.
+        
+        Returns:
+            The user context for the given user_id. None if no user context exists for the given user_id.
+        """
+        user_context = await self.collection.find_one({"userid": user_id})
+        if not user_context:
+            return None
+
+        return UserContext(**user_context)
+
+    async def update_user_context(self, user_id: str, user_context: UserContext) -> UserContext | None:
+        """
+        Update the user context for the given user_id.
+
+        Args:
+            user_id: The user_id for which to update the user context.
+            user_context: The user context to be updated.
+        
+        Raises:
+            UserContextNotFoundError: If no user context exists for the given user_id.
+
+        Returns:
+            The updated user context.
+        """
+        updated_user_context = await self.collection.find_one_and_update(
+            {"userid": user_id}, 
+            {"$set": user_context.model_dump()},
+            return_document=ReturnDocument.AFTER,
+        )
+        if not updated_user_context:
+            raise UserContextNotFoundError(f"User context not found for user_id: {user_id}")
+        
+        return UserContext(**updated_user_context)
